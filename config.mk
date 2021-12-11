284e5aa56e775803d24879954136401a106aa063
Claudio Alessi <smoppy@gmail.com>
2021-12-11 13:19:23 +0100

    Fix POSIX_C_SOURCE macro for snprintf() and clock_gettime()

--- config.mk
+++ config.mk
@@ -12,10 +12,10 @@ INCS =
 LIBS =
 
 # flags
-CPPFLAGS = -D_DEFAULT_SOURCE -D_POSIX_C_SOURCE=2 -DVERSION=\"${VERSION}\"
+CPPFLAGS = -D_DEFAULT_SOURCE -D_POSIX_C_SOURCE=200809L -DVERSION=\"${VERSION}\"
 #CFLAGS   = -g -std=c99 -pedantic -Wall -O0 ${INCS} ${CPPFLAGS}
 CFLAGS   = -std=c99 -pedantic -Wall -Wno-deprecated-declarations -Os ${INCS} ${CPPFLAGS}
-LDFLAGS  = -s ${LIBS}
+LDFLAGS  = ${LIBS}
 
 # Solaris
 #CFLAGS = -fast ${INCS} -DVERSION=\"${VERSION}\"
